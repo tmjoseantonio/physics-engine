6a5c4956bf3245dd44449b21496265f3dad0ec4c
Jose Tovar <tmjoseantonio@gmail.com>
2017-09-27 21:42:48 -0500

    Setup Typescript and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
 const path = require('path');
 
+const DIST_PATH = path.resolve( __dirname, 'dist' );
+const SOURCE_PATH = path.resolve( __dirname, 'src' );
+
 module.exports = {
-  entry: './app/index.js',
+  entry: './app/index.ts',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: DIST_PATH
   },
   module: {
-      rules: [
-          {
-              test: /\.js$/,
-              exclude: [/node_modules/],
-              use: [{
-                  loader: 'babel-loader',
-                  options: {
-                      presets: ['es2015'],
-                  },
-              }],
-          }
-      ]
-    }
+    loaders: [{
+      test: /.ts?$/,
+      loader: 'ts-loader'
+    }],
+    rules: [{
+      test: /\.js$/,
+      exclude: [/node_modules/],
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015'],
+        },
+      }],
+    }]
+  }
 };
